420a4e046e1b7b9e5b9c394e850b926f738a8cf1
CY <ye.chen@bitdeer.com>
2026-04-16 12:12:09 +0800

    pdate

--- rpc/user/userrpc.go
+++ rpc/user/userrpc.go
@@ -16,7 +16,7 @@ import (
 	"google.golang.org/grpc/reflection"
 )
 
-var configFile = flag.String("f", "../../etc/user-all.yaml", "the config file")
+var configFile = flag.String("f", "etc/user-all.yaml", "the config file")
 
 func main() {
 	flag.Parse()
